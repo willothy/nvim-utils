8838e6ce6524e3486effc341c0bdaa589a2416e0
Will Hopkins <willothyh@gmail.com>
2023-03-01 11:14:52 -0800

    chore(tests): work on WIP test runner

--- test-plugin/src/lib.rs
+++ test-plugin/src/lib.rs
@@ -16,9 +16,40 @@ fn get_plugin_info(lua: &Lua, _args: ()) -> LuaResult<LuaTable> {
         .build()
 }
 
+fn get_mode(lua: &Lua) -> Result<(), String> {
+    Ok(())
+}
+
+tests! {
+    "Get Mode": get_mode
+}
+
+const TEST_SEP: &str = "\n--------------------------\n";
+
+fn run_tests(lua: &Lua) -> Result<(), Vec<(&'static str, String)>> {
+    let results = tests
+        .iter()
+        .filter_map(|test| test.run(lua).err())
+        .collect::<Vec<_>>();
+    if results.is_empty() {
+        Ok(())
+    } else {
+        Err(results)
+    }
+}
+
 /// The #[lua_module] attribute generates an entry point for the plugin.
 #[mlua::lua_module]
 pub fn test_plugin(lua: &Lua) -> LuaResult<LuaTable> {
+    if let Err(e) = run_tests(lua) {
+        let msg = e
+            .iter()
+            .map(|(n, e)| format!("{}: {}", n, e))
+            .collect::<Vec<_>>()
+            .join(TEST_SEP);
+        assert!(false, "API Tests failed:\n{}", msg)
+    }
+
     // Create a new module builder
     ModuleBuilder::new(lua)
         // Add the hello function to the module
@@ -27,4 +58,36 @@ pub fn test_plugin(lua: &Lua) -> LuaResult<LuaTable> {
         .with_fn("get_plugin_info", get_plugin_info)?
         // Build the module
         .build()
+}
+
+struct Test {
+    runner: fn(&Lua) -> Result<(), String>,
+    name: &'static str,
+}
+
+impl Test {
+    fn run(&self, lua: &Lua) -> Result<(), (&'static str, String)> {
+        if let Err(e) = (self.runner)(lua) {
+            return Err((self.name, e));
+        }
+        Ok(())
+    }
+}
+
+#[macro_export]
+macro_rules! count {
+    () => (0usize);
+    ( $x:tt $($xs:tt)* ) => (1usize + count!($($xs)*));
+}
+
+#[macro_export]
+macro_rules! tests {
+    ($($name:literal: $r:expr)*) => {
+		static tests: [Test; count!($($name)*)] = [
+			$(Test {
+				name: $name,
+				runner: $r
+			}),*
+		];
+	};
 }
